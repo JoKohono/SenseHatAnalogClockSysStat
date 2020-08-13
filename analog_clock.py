c38f83d48679d97384298908c86021072696c084
Scott McCoy <strek.ncc1701a@gmail.com>
2020-08-13 16:48:36 -0700

    blue 2-dots second ring: good yellow-red static watchface: good reference before going further

--- analog_clock.py
+++ analog_clock.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from sense_hat import SenseHat
 #from sense_emu import SenseHat
 import time, random
@@ -5,18 +6,19 @@ import time, random
 sleeptime_l=1.0
 sleeptime_m=sleeptime_l/2
 sleeptime_s=sleeptime_m/2
+sleeptime_watchtick = 1
 
 s = SenseHat()
 s.low_light = True
 s.rotation = 90
 
-green = (0, 255, 0)
-yellow = (255, 255, 0)
-blue = (0, 0, 255)
-red = (255, 0, 0)
-white = (255, 255, 255)
-nothing = (0, 0, 0)
-pink = (255,105, 180)
+G = green = (0, 255, 0)
+Y = yellow = (255, 255, 0)
+B = blue = (0, 0, 255)
+R = red = (255, 0, 0)
+W = white = (255, 255, 255)
+O = nothing = (0, 0, 0)
+P = pink = (255,105, 180)
 
 # []
 
@@ -26,70 +28,53 @@ sec_stripe_length = len(sec_stripeX)
 sec_true = 0  #will effectively be between 1 and 60
 sec_LED_current = sec_true*sec_stripe_length/60
 
-def secondsarray():
-    off = nothing
-    on = yellow
-    secondslogo =[
+watchface_1 = [
     O, O, O, O, O, O, O, O,
+    O, O, O, Y, Y, O, O, O,
     O, O, O, O, O, O, O, O,
+    O, Y, O, R, R, O, Y, O,
+    O, Y, O, R, R, O, Y, O,
     O, O, O, O, O, O, O, O,
-    O, O, O, O, P, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
+    O, O, O, Y, Y, O, O, O,
     O, O, O, O, O, O, O, O,
     ]
-    return secondslogo
 
-def ping():
-    P = pink
-    O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, P, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-    return logo
 
-def pong():
-    P = pink
-    O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, P, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-    return logo
+# def ping():
+#     P = pink
+#     O = nothing
+#     logo = [
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, P, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     O, O, O, O, O, O, O, O,
+#     ]
+#     return logo
 
-images = [ping, pong]
-count = 0
+# 
+# images = [ping, pong]
+
 
 while True: 
-#    s.set_pixels(images[count % len(images)]())
-#    time.sleep(sleeptime_m)
+    s.set_pixels(watchface_1)
+    time.sleep(sleeptime_l)
 
-    s.clear(nothing)
-    sec_true = 0
+    localtime = time.localtime(time.time())
+    sec_true = localtime.tm_sec
     while sec_true < 60:
+        localtime = time.localtime(time.time())
+        sec_true = localtime.tm_sec
         sec_LED_current = int(sec_true * (sec_stripe_length / 60))
         sec_stripe_X_LED = int(sec_stripeX[sec_LED_current])
         sec_stripe_Y_LED = int(sec_stripeY[sec_LED_current])
-        print(sec_true)
-        print(sec_stripe_X_LED, " - ", sec_stripe_Y_LED)
-        s.set_pixel((sec_stripeX[sec_LED_current]), sec_stripeY[sec_LED_current], blue)
+        print(sec_true, ": = LED: ", sec_stripe_X_LED, " - ", sec_stripe_Y_LED)
+        s.set_pixel((sec_stripeX[sec_LED_current]), sec_stripeY[sec_LED_current], blue) 
+        s.set_pixel((sec_stripeX[sec_LED_current-2]), sec_stripeY[sec_LED_current-2], nothing)       
+        time.sleep(sleeptime_watchtick/2)
+        s.set_pixel((sec_stripeX[sec_LED_current-1]), sec_stripeY[sec_LED_current-1], nothing)
 
-        time.sleep(sleeptime_l)
-        sec_true += 1
-
-#   count += 1
 s.clear(nothing)
